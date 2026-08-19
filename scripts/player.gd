e0d518c0327dbdb40b61404192c9bb0a1efaf439
monttggz <flaviogouveajunior@gmail.com>
2026-08-19 08:55:15 -0300

    mudancas

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 ## Player do jogo de plataforma.
 
-var SPEED: float = 200.0
+const SPEED: float = 200.0
 var JUMP_VELOCITY: float = -400.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -15,13 +14,11 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	var direction: float = Input.get_axis("move_left", "move_right")
-	var pode_andar: bool = animated_sprite.sprite_frames.has_animation("run")
-	if direction != 0.0 and pode_andar:
+	if direction != 0.0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 
-	_update_animation(direction, pode_andar)
 	move_and_slide()
 func brilhar() -> void:
 	$Particulas.restart()
@@ -31,7 +28,7 @@ func _update_animation(direction: float, pode_andar: bool) -> void:
 
 	if not is_on_floor():
 		animated_sprite.play("jump")
-	elif direction != 0.0 and pode_andar:
+	elif direction != 0.0:
 		animated_sprite.play("run")
 	else:
 		animated_sprite.play("idle")
